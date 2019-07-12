ab3f4775483451f5ee9148830d88bdabb55fd2cb
Yordan Arnaudov <yarnaudov@codix.bg>
2019-07-12 17:50:03 +0300

    Refactor docker configuration

--- docker/node/entrypoint.sh
+++ docker/node/entrypoint.sh
@@ -4,9 +4,14 @@ set -e
 
 ENV=${APP_ENV:-local}
 
+echo "Starting ssh deamon..."
+sudo /etc/init.d/ssh start
+
 if [ ${ENV} = "local" ]; then
 
     echo "Running the vue ui..."
-    vue ui --host 0.0.0.0 --port 8000 --headless
+    exec vue ui --host 0.0.0.0 --port 8000 --headless
 
 fi
+
+exec "$@"
