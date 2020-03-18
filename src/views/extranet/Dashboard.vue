690559f0a853ce6d355b284c4f579cc78c57ce72
Yordan Arnaudov <yarnaudov@codix.bg>
2020-03-18 15:50:16 +0200

    Fix extranet dashboard

--- src/views/extranet/Dashboard.vue
+++ src/views/extranet/Dashboard.vue
@@ -1,58 +1,71 @@
 <template>
   <div class="extranet">
     <div v-if="$route.meta.name === 'extranet'" class="row">
-      <div class="col s12 20">
-      <div class="card">
-        <div class="card-content">
-          <span class="card-title">My Extranet Builds</span>
-          <Builds :containers="userContainers" ></Builds>
+      <div class="col s12 l6">
+        <div class="card" ref="my_builds">
+          <div class="card-content">
+            <span class="card-title">My Extranet Builds</span>
+            <Builds :containers="userContainers" ></Builds>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col s12 l6" v-if="loadedBranchBuilds">
-        <div class="card">
+      <div class="col s12 l6">
+        <div class="card" ref="builds_by_branch">
+          <div class="card-content">
+            <span class="card-title">Extranet Builds By Branch</span>
+            <table>
+              <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Builds</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="(container, index) in containersGroupedByBranch" :key="index">
+                <td>{{ index + 1 }}</td>
+                <td>
+                  <router-link
+                    v-bind:to="'/extranet/branches/' + encodeURIComponent(container.branch)">
+                    {{ container.branch }}
+                  </router-link>
+                </td>
+                <td>{{ container.builds }}</td>
+              </tr>
+              <tr v-if="containersGroupedByBranch.length === 0">
+                <td colspan="3">There are no builds</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 l6">
+        <div class="card" ref="stats_by_branch">
             <div class="card-content">
-                <div class="row">
-                    <div class="col s12 l4 right">
-                        <div class="input-field">
-                            <Select class="col s12"
-                                :select="selectStartDate"
-                                @selectedVal="selectedStartDateBranch"
-                            />
-                        </div>
-                    </div>
+              <span class="card-title">Extranet builds by branch</span>
+              <div class="col s12 l4 right">
+                <div class="input-field">
+                  <Select class="col s12" :select="selectStartDate" @selectedVal="getBranchStatistics"/>
                 </div>
-                <div class="row">
-                    <div class="charts col s12 l11">
-                      <span class="card-title">Extranet builds by branch</span>
-                      <BarChart :chart-data="dataCollectionBranches" :options="options"></BarChart>
-                    </div>
-                </div>
+              </div>
+              <BarChart :data="branchesChartData" :options="chartOptions"></BarChart>
             </div>
         </div>
-    </div>
-    <div class="col s12 l6" v-if="loadedUsersBuilds">
-        <div class="card">
+      </div>
+      <div class="col s12 l6">
+        <div class="card" ref="stats_by_user">
             <div class="card-content">
-                <div class="row">
-                    <div class="col s12 l4 right">
-                        <div class="input-field">
-                            <Select class="col s12"
-                                :select="selectStartDate"
-                                @selectedVal="selectedStartDateUsers"
-                            />
-                        </div>
-                    </div>
+              <span class="card-title">Extranet builds by user</span>
+              <div class="col s12 l4 right">
+                <div class="input-field">
+                  <Select class="col s12" :select="selectStartDate" @selectedVal="getUserStatistics"/>
                 </div>
-                <div class="row">
-                    <div class="charts col s12 l11">
-                        <span class="card-title">Extranet builds by user</span>
-                        <BarChart :chart-data="dataCollectionUsers" :options="options"></BarChart>
-                    </div>
-                </div>
+              </div>
+              <BarChart :data="usersChartData" :options="chartOptions"></BarChart>
             </div>
         </div>
-    </div>
+      </div>
     </div>
     <router-view v-else :key="$route.path"/>
   </div>
@@ -70,8 +83,6 @@ export default {
   data() {
     return {
       startDate: 30,
-      loadedBranchBuilds: false,
-      loadedUsersBuilds: false,
       selectStartDate: {
         id: 'startDate_select',
         name: 'startDate',
@@ -96,55 +107,10 @@ export default {
           value: 30,
         },
       },
-    };
-  },
-  computed: {
-    dataCollectionBranches() {
-      const builds = this.buildsGroupedByBranch();
-
-      const dataCollection = {
-        labels: Object.keys(builds),
-        datasets: [
-          {
-            label: 'BUILDS',
-            data: Object.values(builds),
-            backgroundColor: [],
-          },
-        ],
-      };
-
-      try {
-        dataCollection.datasets[0].data.forEach(() => {
-          dataCollection.datasets[0].backgroundColor.push('#4da6ff');
-        });
-      } catch (error) { console.log('Could not get colour for table'); }
-
-      return dataCollection;
-    },
-    dataCollectionUsers() {
-      const builds = this.buildsGroupedByUser();
-
-      const dataCollection = {
-        labels: Object.keys(builds),
-        datasets: [
-          {
-            label: 'BUILDS',
-            data: Object.values(builds),
-            backgroundColor: ['#dd99ff'],
-          },
-        ],
-      };
-
-      try {
-        dataCollection.datasets[0].data.forEach(() => {
-          dataCollection.datasets[0].backgroundColor.push(this.getRandomColour());
-        });
-      } catch (error) { console.log('Could not get random colours for table'); }
-
-      return dataCollection;
-    },
-    options() {
-      const optionsCollection = {
+      chartOptions: {
+        legend: {
+          display: false,
+        },
         scales: {
           yAxes: [{
             ticks: {
@@ -152,45 +118,73 @@ export default {
             },
           }],
         },
-      };
-
-      return optionsCollection;
-    },
+      },
+    };
+  },
+  computed: {
     userContainers() {
       return this.$store.getters['extranet/getContainersByUser'](
         this.$auth.getUser().username,
       );
     },
+    containersGroupedByBranch() {
+      return this.$store.getters['extranet/getContainersGroupedByBranch']();
+    },
+    branchesChartData() {
+      let builds = {};
+      this.$store.getters['builds/getBuildsGroupedByBranch']('extranet').forEach((build) => {
+        builds[build.branch] = build.builds;
+      });
+
+      return {
+        labels: Object.keys(builds),
+        datasets: [
+          {
+            label: 'Builds',
+            data: Object.values(builds),
+          },
+        ],
+      };
+    },
+    usersChartData() {
+      let builds = {};
+      this.$store.getters['builds/getBuildsGroupedByUser']('extranet').forEach((build) => {
+        builds[build.user] = build.builds;
+      });
+
+      return {
+        labels: Object.keys(builds),
+        datasets: [
+          {
+            label: 'Builds',
+            data: Object.values(builds)
+          },
+        ],
+      };
+    },
   },
   methods: {
     getContainers() {
-      this.$store.dispatch('extranet/getContainers');
-    },
-    buildsGroupedByBranch() {
-      const builds = {};
-
-      this.$store.getters['builds/getBuildsGroupedByBranch']('extranet').forEach((groupedBuild) => {
-        builds[groupedBuild.branch] = groupedBuild.builds;
+      const loader1 = this.$loading.show({ container: this.$refs.my_builds });
+      const loader2 = this.$loading.show({ container: this.$refs.builds_by_branch });
+      this.$store.dispatch('extranet/getContainers').then(() => {
+        loader1.hide();
+        loader2.hide();
       });
-
-      return builds;
     },
-    buildsGroupedByUser() {
-      const builds = {};
-
-      this.$store.getters['builds/getBuildsGroupedByUser']('extranet').forEach((groupedBuild) => {
-        builds[groupedBuild.user] = groupedBuild.builds;
-      });
-
-      return builds;
+    getBranchStatistics(days = {}) {
+      const loader = this.$loading.show({container: this.$refs.stats_by_branch});
+      this.$store.dispatch(
+        'builds/getBuildsForPeriod',
+        {startDate: this.getStartDate(days.value || this.startDate), stateName: 'branchBuilds'},
+      ).then(() => loader.hide());
     },
-    getRandomColour() {
-      const letters = '0123456789ABCDEF'.split('');
-      let colour = '#';
-      for (let i = 0; i < 6; i += 1) {
-        colour += letters[Math.floor(Math.random() * 16)];
-      }
-      return colour;
+    getUserStatistics(days = {}) {
+      const loader = this.$loading.show({ container: this.$refs.stats_by_user});
+      this.$store.dispatch(
+        'builds/getBuildsForPeriod',
+        { startDate: this.getStartDate(days.value || this.startDate), stateName: 'usersBuilds' },
+      ).then(() => loader.hide());
     },
     getStartDate(value) {
       const newDate = new Date(
@@ -199,38 +193,11 @@ export default {
 
       return Math.round(new Date(newDate).getTime() / 1000);
     },
-    selectedStartDateBranch(value) {
-      this.$store.dispatch(
-        'builds/getBuildsForPeriod',
-        { startDate: this.getStartDate(value.value), stateName: 'branchBuilds' },
-      );
-    },
-    selectedStartDateUsers(value) {
-      this.$store.dispatch(
-        'builds/getBuildsForPeriod',
-        { startDate: this.getStartDate(value.value), stateName: 'usersBuilds' },
-      );
-    },
-    async prepareData() {
-      await this.getContainers();
-
-      await this.$store.dispatch(
-        'builds/getBuildsForPeriod',
-        { startDate: this.getStartDate(this.startDate), stateName: 'branchBuilds' },
-      ).then(() => {
-        this.loadedBranchBuilds = true;
-      });
-
-      await this.$store.dispatch(
-        'builds/getBuildsForPeriod',
-        { startDate: this.getStartDate(this.startDate), stateName: 'usersBuilds' },
-      ).then(() => {
-        this.loadedUsersBuilds = true;
-      });
-    },
   },
   mounted() {
-    this.prepareData();
+    this.getContainers();
+    this.getBranchStatistics();
+    this.getUserStatistics();
   },
 };
 </script>
