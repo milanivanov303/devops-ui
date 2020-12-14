7c1f806e0356ee2859ea347cf9ebffc8bd8cd5ea
Yordan Arnaudov <yarnaudov@codix.bg>
2020-12-14 13:44:50 +0200

    Fix nginx reload

--- docker/nginx/reload.sh
+++ docker/nginx/reload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Check for changed files last 20 seconds
-MODIFIED=$(find /etc/nginx/conf.d/proxy/ -mmin -1 | wc -l)
+export MODIFIED=$(find /etc/nginx/conf.d/proxy/ -mmin -1 | wc -l)
 
 # If there are changed files reload nginx configuration
 if [ ${MODIFIED} -gt 0 ]; then
