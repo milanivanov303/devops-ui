efd8b033091a6a573fd3b36b030a8caa027206da
Yordan Arnaudov <yarnaudov@codix.bg>
2021-01-14 16:44:16 +0200

    Save point

---
+++ src/store/modules/imx_be/actions.js
@@ -0,0 +1,37 @@
+// https://vuex.vuejs.org/en/actions.html
+export default {
+  getBranches({ commit }) {
+    const promise = api('devops').get('imx-be/branches');
+
+    promise
+      .then(response => commit('branches', response.data))
+      .catch(() => commit('error', 'Could not get branches list'));
+
+    return promise;
+  },
+
+  getHost({ commit }) {
+    const promise = api('devops').get('imx-be/host');
+
+    promise
+      .then((response) => {
+        commit('host', response.data.host);
+      })
+      .catch(() => commit('error', 'Could not get host name', { root: true }));
+    return promise;
+  },
+
+  startBuild({ commit }, payload) {
+    const promise = api('devops').post('imx-be/build', payload);
+    promise.catch(error => commit('error', error));
+    return promise;
+  },
+
+  removeBuild({ commit }, id) {
+    const promise = api('devops').delete(`imx-be/build/${id}`);
+    promise
+      .then(() => commit('removeBuild', id))
+      .catch(error => commit('error', error, { root: true }));
+    return promise;
+  },
+};
