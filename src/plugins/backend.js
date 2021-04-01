c1225a3a5ac2e12aae5c77b36d11f923362f9bdc
Gergana Dimitrova <gdimitrova@codix.eu>
2021-04-01 21:40:51 +0300

    Use only config as param for ApiSdk and update ea-js-sdk version

--- src/plugins/backend.js
+++ src/plugins/backend.js
@@ -2,15 +2,7 @@ import ApiSdk from '@enterpriseapps/ea-js-sdk';
 import config from '../../config';
 
 // Main api sdk instance. It is used for authentication and requests with logged in user
-const apiSdk = new ApiSdk(
-  config,
-  config.um.url,
-  config.auth.code,
-  {
-    session_name: config.auth.session_name,
-    session_expire: config.auth.session_expire,
-  },
-);
+const apiSdk = new ApiSdk(config);
 
 window.auth = apiSdk.auth;
 
@@ -19,17 +11,7 @@ window.api = name => apiSdk.api(config[name].url, config[name].code);
 // Auto api sdk instance. It is used for requests when no user is logged into the system
 const apiSdkAuto = () => {
   if (!window.apiSdkAuto) {
-    window.apiSdkAuto = new ApiSdk(
-      config,
-      config.um.url,
-      config.auth.code,
-      {
-        session_name: config.auth_auto.session_name,
-        session_expire: config.auth.session_expire,
-        username: config.auth_auto.username,
-        password: config.auth_auto.password,
-      },
-    );
+    window.apiSdkAuto = new ApiSdk(config);
   }
   return window.apiSdkAuto;
 };
