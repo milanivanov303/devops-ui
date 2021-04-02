42b4969e69a1f434335c8fce8d9e819517191728
Gergana Dimitrova <gdimitrova@codix.bg>
2021-04-02 18:28:09 +0300

    Send only needed options to ApiSdk instead of config object

--- src/plugins/backend.js
+++ src/plugins/backend.js
@@ -2,7 +2,13 @@ import ApiSdk from '@enterpriseapps/ea-js-sdk';
 import config from '../../config';
 
 // Main api sdk instance. It is used for authentication and requests with logged in user
-const apiSdk = new ApiSdk(config);
+const apiSdk = new ApiSdk({
+  url: config.um.url,
+  code: config.auth.code,
+  applications: config.auth.applications,
+  session_name: config.auth_auto.session_name,
+  session_expire: config.auth.session_expire,
+});
 
 window.auth = apiSdk.auth;
 
@@ -11,7 +17,15 @@ window.api = name => apiSdk.api(config[name].url, config[name].code);
 // Auto api sdk instance. It is used for requests when no user is logged into the system
 const apiSdkAuto = () => {
   if (!window.apiSdkAuto) {
-    window.apiSdkAuto = new ApiSdk(config);
+    window.apiSdkAuto = new ApiSdk({
+      url: config.um.url,
+      code: config.auth.code,
+      applications: config.auth.applications,
+      session_name: config.auth_auto.session_name,
+      session_expire: config.auth.session_expire,
+      username: config.auth_auto.username,
+      password: config.auth_auto.password,
+    });
   }
   return window.apiSdkAuto;
 };
