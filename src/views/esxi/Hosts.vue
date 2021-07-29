cf3c6bb902b6bc19e8132610601cb422b49f7ac1
Milan Ivanov <mrivanov@codix.bg>
2021-07-29 16:06:47 +0300

    Change update with background process and expand search functionality lint

--- src/views/esxi/Hosts.vue
+++ src/views/esxi/Hosts.vue
@@ -118,24 +118,26 @@ export default {
         let esxiHostsResults = esxiHosts.filter((esxiHost) => esxiHost.hostname.match(regexp));
 
         if (esxiHostsResults.length === 0) {
-          esxiHostsResults = esxiHosts.filter(function(esxiHost) {
+          esxiHostsResults = esxiHosts.filter((esxiHost) => {
             if (esxiHost.vms_details) {
-              return esxiHost.vms_details.some((vm) =>
-                vm.powered && vm.powered !== 'off' && vm.main_info.name.match(regexp));
+              return esxiHost.vms_details.some((vm) => vm.powered && vm.powered !== 'off' && vm.main_info.name.match(regexp));
             }
+            return false;
           });
 
           if (esxiHostsResults.length === 0) {
-            esxiHostsResults = esxiHosts.filter(function(esxiHost) {
+            esxiHostsResults = esxiHosts.filter((esxiHost) => {
               if (esxiHost.vms_details) {
-                return esxiHost.vms_details.some(function(vm) {
+                return esxiHost.vms_details.some((vm) => {
                   if (vm.powered && vm.powered !== 'off'
                     && vm.instances
                     && vm.instances instanceof Array) {
                     return vm.instances.some((instance) => instance.name.match(regexp));
                   }
+                  return false;
                 });
               }
+              return false;
             });
           }
         }
