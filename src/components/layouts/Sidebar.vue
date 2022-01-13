9ad06ec0944a6a8f62c9f91b5b4026378e855d9c
Kristian Zlatkov <kzlatkov@codix.eu.com>
2022-01-13 14:26:35 +0200

    Made a new submenu in the Sidenav named "PAS" The new order is: PAS  DevOps Matrix  SOA Modification  Extranet    Dashboard    Branches  Debiteur    Dashboard    Branches
    Made a new module in store.Folder is named pas.
    Added needed files and copied the content from Extranet for configurations and soa modifications since they should not be under extranet.
    Changed the routes since configurations and soa-modifications are under "pas/" route now.
    
    Added the new menu in Sidebar.vue.

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -1,52 +1,75 @@
 <template>
   <ul id="nav-mobile" class="sidenav sidenav-fixed">
     <li class="logo">
-        <img src="/applications/yTjvXuxGM5i/big_logo" alt="DevOps Management">
+      <img src="/applications/yTjvXuxGM5i/big_logo" alt="DevOps Management">
+    </li>
+    <li :class="{ active: isActive('dashboard') }">
+      <router-link to="/dashboard">
+        <i class="material-icons">web</i> Dashboard
+      </router-link>
     </li>
     <li>
       <ul class="collapsible collapsible-accordion">
-        <li :class="{ active: isActive('dashboard') }">
-          <router-link to="/dashboard">
-            <i class="material-icons">web</i> Dashboard
-          </router-link>
-        </li>
-        <li :class="{ active: isActive('extranet') }">
+        <li v-bind:class="{
+      active:
+        isActive('pas') || isActive('extranet') || isActive('debiteur')}"
+        >
           <a class="collapsible-header">
-            <i class="material-icons">laptop_chromebook</i> Extranet
+            <i class="material-icons">business_center</i> PAS
             <i class="material-icons right">arrow_drop_down</i>
           </a>
           <div class="collapsible-body">
-            <ul>
-              <li :class="{ active: isActive('extranet/dashboard') }">
-                <router-link to="/extranet/dashboard"> Dashboard</router-link>
+            <ul class="collapsible">
+              <li :class="{ active: isActive('pas/configurations') }">
+                <router-link to="/pas/configurations"> DevOps Matrix</router-link>
               </li>
-              <li v-bind:class="{ active: isActive('extranet/branches') }">
-                <router-link to="/extranet/branches"> Branches</router-link>
+              <li :class="{ active: isActive('pas/soa-modification') }">
+                <router-link to="/pas/soa-modification"> SOA Modification</router-link>
               </li>
-              <li :class="{ active: isActive('extranet/configurations') }">
-                <router-link to="/extranet/configurations"> DevOps Matrix</router-link>
+              <li>
+                <a
+                  class="collapsible-header-pas"
+                  @click="
+                  (showExtranetAccordion = !showExtranetAccordion) &&
+                  (showDebiteurAccordion = false)"
+                >
+                  <i class="material-icons white-text">laptop_chromebook</i> Extranet
+                  <i class="material-icons white-text right">arrow_drop_down</i>
+                </a>
+                <div class="collapsible-body" v-if="showExtranetAccordion">
+                  <ul>
+                    <li :class="{ active: isActive('extranet/dashboard') }">
+                      <router-link to="/extranet/dashboard"> Dashboard</router-link>
+                    </li>
+                    <li :class="{ active: isActive('extranet/branches') }">
+                      <router-link to="/extranet/branches"> Branches</router-link>
+                    </li>
+                  </ul>
+                </div>
               </li>
-              <li :class="{ active: isActive('extranet/soa-modification') }">
-                <router-link to="/extranet/soa-modification"> SOA Modification</router-link>
-              </li>
-              <li><div class="divider"></div></li>
             </ul>
-          </div>
-        </li>
-        <li :class="{ active: isActive('debiteur') }">
-          <a class="collapsible-header">
-            <i class="material-icons">laptop_chromebook</i> Debiteur
-            <i class="material-icons right">arrow_drop_down</i>
-          </a>
-          <div class="collapsible-body">
-            <ul>
-              <li :class="{ active: isActive('debiteur/dashboard') }">
-                <router-link to="/debiteur/dashboard"> Dashboard</router-link>
+            <ul class="collapsible">
+              <li>
+                <a
+                  class="collapsible-header-pas"
+                  @click="(showDebiteurAccordion = !showDebiteurAccordion)
+                   && (showExtranetAccordion = false)"
+                >
+                  <i class="material-icons white-text">laptop_chromebook</i> Debiteur
+                  <i class="material-icons white-text right">arrow_drop_down</i>
+                </a>
+                <div class="collapsible-body" v-if="showDebiteurAccordion">
+                  <ul>
+                    <li :class="{ active: isActive('debiteur/dashboard') }">
+                      <router-link to="/debiteur/dashboard"> Dashboard</router-link>
+                    </li>
+                    <li :class="{ active: isActive('debiteur/branches') }">
+                      <router-link to="/debiteur/branches"> Branches</router-link>
+                    </li>
+                    <li><div class="divider"></div></li>
+                  </ul>
+                </div>
               </li>
-              <li v-bind:class="{ active: isActive('debiteur/branches') }">
-                <router-link to="/debiteur/branches"> Branches</router-link>
-              </li>
-              <li><div class="divider"></div></li>
             </ul>
           </div>
         </li>
@@ -120,11 +143,11 @@
               </li>
               <li :class="{ active: isActive('cms/templates') }">
                 <router-link to="/cms/templates">
-                   Templates
+                  Templates
                 </router-link>
               <li :class="{ active: isActive('cms/inventory') }">
                 <router-link to="/cms/inventory">
-                   Inventory
+                  Inventory
                 </router-link>
               </li>
               <li :class="{ active: isActive('cms/modification') }">
@@ -242,6 +265,12 @@
 import config from '@/config';
 
 export default {
+  data() {
+    return {
+      showExtranetAccordion: false,
+      showDebiteurAccordion: false,
+    };
+  },
   methods: {
     isActive(path) {
       const regexp = RegExp(`^/${path}`);
@@ -256,26 +285,41 @@ export default {
     getApplicationCode(application) {
       return config[application].code;
     },
+
+    showPasAccordeonMenu() {
+      if (this.isActive('extranet')) {
+        this.showExtranetAccordion = true;
+      }
+      if (this.isActive('debiteur')) {
+        this.showDebiteurAccordion = true;
+      }
+    },
   },
 
   mounted() {
     this.$M.Sidenav.init(document.querySelector('.sidenav'));
     this.$M.Collapsible.init(document.querySelector('.collapsible'));
+    this.showPasAccordeonMenu();
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  li.active {
-    .collapsible-body {
-      display: block;
+li.active {
+  .collapsible-body {
+    display: block;
+  }
+  .collapsible-body:hover {
+    .collapsible-header-pas {
+      color: white !important;
     }
   }
+}
 
-  .sidenav {
-    .subheader {
-      color: white;
-      padding: 0 15px;
-    }
+.sidenav {
+  .subheader {
+    color: white;
+    padding: 0 15px;
   }
+}
 </style>
---
+++ src/store/modules/pas/mutations.js
@@ -0,0 +1,26 @@
+// https://vuex.vuejs.org/en/mutations.html
+
+import Vue from 'vue';
+
+export default {
+  configurations(state, configurations) {
+    state.configurations = configurations;
+  },
+  createConfiguration(state, configuration) {
+    const { configurations } = state;
+    configurations.push(configuration);
+    Vue.set(state, 'configurations', configurations);
+  },
+  updateConfiguration(state, configuration) {
+    state.configurations.map((d) => {
+      if (d.id === configuration.id) {
+        Vue.set(state.configurations, state.configurations.indexOf(d), configuration);
+      }
+      return state.configurations;
+    });
+  },
+  deleteConfiguration(state, id) {
+    const configurations = state.configurations.filter((d) => d.id !== id);
+    Vue.set(state, 'configurations', configurations);
+  },
+};
