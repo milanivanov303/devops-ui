bc9396bae1ad423ee03aca3e2e3748e5a180f07b
gdimitrova <gdimitrova@codix.eu>
2022-03-01 11:59:51 +0200

    Get the powered VM from route query

--- src/store/modules/esxi/getters.js
+++ src/store/modules/esxi/getters.js
@@ -17,7 +17,7 @@ export default {
 
     return host.virtual_machines.reduce((accumulator, currentValue) => {
       let sum = accumulator;
-      if (currentValue.powered === 'on') {
+      if (currentValue.powered === 'on' && currentValue.hardware) {
         sum += currentValue.hardware.memory;
       }
       return sum;
@@ -42,7 +42,7 @@ export default {
   getTotalMemoryByHosts: (state) => {
     const memoryByHosts = [];
     state.esxiHosts.forEach((esxiHost) => {
-      if (esxiHost.memory.physical_memory) {
+      if (esxiHost.memory && esxiHost.memory.physical_memory) {
         memoryByHosts.push(Math.round(esxiHost.memory.physical_memory / 1024 ** 3));
       } else {
         memoryByHosts.push(0);
