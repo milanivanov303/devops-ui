159e4eccd63ca0c33c473b603fe25b3726b796ca
Yordan Arnaudov <yarnaudov@codix.bg>
2022-04-13 12:12:23 +0300

    Add extranet x4

---
+++ src/views/extranet-x4/components/Build.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="row">
+    <div class="col s12" >
+      <button
+        v-if="$auth.can('extranet.create-builds')"
+        class="btn"
+        @click="open()">
+        <i class="material-icons left">add</i> New build
+      </button>
+
+      <Modal v-if="showModal" @close="close()" class="right-sheet">
+        <template v-slot:header>{{ branch }} // Create new build </template>
+        <template v-slot:content>
+          <div v-if="!build.started" class="col s12 l11" key="form" >
+            <div class="row" v-if="!branch">
+              <div class="col s12" >
+                <Autocomplete
+                  label="Branch"
+                  icon="dynamic_feed"
+                  :items="branches"
+                  v-model="form.branch"
+                  :invalid="$v.form.branch.$error"
+                  @blur="$v.form.branch.$touch()"
+                />
+              </div>
+              <div class="validator col s11 offset-s1">
+                <div class="red-text" v-if="$v.form.branch.$error">
+                  <p v-if="!$v.form.branch.required">
+                    Branch field must not be empty.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12" >
+                <Select
+                  :options="images"
+                  icon="cloud_upload"
+                  label="Deploy on"
+                  displayed="label"
+                  :default-option="false"
+                  v-model="form.image"
+                />
+              </div>
+            </div>
+          </div>
+          <BuildProgress
+            v-else
+            :broadcast="build.broadcast"
+            :status="build.status"
+            :summary="build.summary"
+            :error="build.error"
+          />
+        </template>
+        <template v-slot:footer>
+          <button v-if="!build.started" class="waves-effect btn" @click="start()">
+            <i class="material-icons left">play_arrow</i> Start
+          </button>
+        </template>
+      </Modal>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { required } from 'vuelidate/lib/validators';
+import BuildProgress from '@/components/BuildProgress';
+import EventBus from '@/event-bus';
+
+function initialState() {
+  return {
+    showModal: false,
+    form: {
+      branch: null,
+      image: null,
+    },
+    build: {
+      started: false,
+      status: '',
+      summary: '',
+      error: null,
+      broadcast: null,
+    },
+  };
+}
+
+export default {
+  components: {
+    BuildProgress,
+  },
+
+  props: {
+    branch: String,
+  },
+
+  data() {
+    return initialState();
+  },
+
+  computed: {
+    instances() {
+      return this.$store.state.mmpi.instances;
+    },
+    branches() {
+      return this.$store.state['extranet-x4'].branches;
+    },
+    images() {
+      return this.$store.state['extranet-x4'].images;
+    },
+  },
+
+  validations() {
+    const validations = {
+      form: {},
+    };
+
+    if (!this.branch) {
+      validations.form.branch = {
+        required,
+        name: {
+          required,
+        },
+      };
+    }
+
+    return validations;
+  },
+
+  watch: {
+    images() {
+      [this.form.image] = this.images;
+    },
+  },
+
+  methods: {
+    getData() {
+      this.$store.dispatch('extranet-x4/getBranches');
+      this.$store.dispatch('extranet-x4/getImages');
+    },
+
+    open() {
+      this.form = initialState().form;
+      this.build = initialState().build;
+
+      this.getData();
+
+      this.showModal = true;
+    },
+
+    close() {
+      this.showModal = false;
+      this.$v.$reset();
+    },
+
+    start() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
+
+      this.$store.dispatch('extranet-x4/startBuild', {
+        branch: this.form.branch ? this.form.branch.name : this.branch,
+        image: this.form.image,
+      })
+        .then((response) => {
+          this.build.status = 'running';
+          this.build.summary = 'Build will start shortly ...';
+          this.build.broadcast = response.data.broadcast;
+          EventBus.$emit('build.created');
+        })
+        .catch((error) => {
+          this.build.status = 'failed';
+          this.build.summary = 'Could not start build';
+          if (error.response.status === 403) {
+            this.build.error = 'You do not have insufficient rights to create build';
+          } else {
+            this.build.error = error;
+          }
+        })
+        .finally(() => { this.build.started = true; });
+    },
+  },
+};
+</script>
---
+++ src/views/extranet-x4/components/BuildInfo.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="col s12 l11">
+      <div class="row">
+        <TextInput
+          class="col s12 readonly"
+          label="Branch"
+          icon="laptop_chromebook"
+          :value="build.details.branch"
+        />
+      </div>
+      <div class="row" v-if="build.status === 'running'">
+        <div class="input-field col s12">
+          <i class="material-icons prefix">link</i>
+          <a :href="build.details.url" target="_blank" rel="noopener noreferrer">
+            <input id="deploy-url"
+                   class="readonly"
+                   type="text"
+                   :value="build.details.url">
+          </a>
+          <label :class="{active: build.details.url}" for="deploy-url">Deploy URL</label>
+        </div>
+
+      </div>
+      <div class="row" v-if="build.details.image">
+        <TextInput
+          class="col s12 readonly"
+          label="Docker image"
+          icon="adjust"
+          :value="build.details.image.label"
+        />
+      </div>
+      <div class="row">
+        <TextInput
+            class="col s12 m6 readonly"
+            label="Created on"
+            icon="event"
+            :value="build.created_on"
+        />
+        <TextInput
+            class="col s12 m6 readonly"
+            label="Created by"
+            icon="person"
+            :value="build.created_by"
+        />
+      </div>
+      <div class="row" v-if="build.removed_on">
+        <TextInput
+            class="col s12 m6 readonly"
+            label="Removed on"
+            icon="event"
+            :value="build.removed_on"
+        />
+        <TextInput
+            class="col s12 m6 readonly"
+            label="Removed by"
+            icon="person"
+            :value="build.removed_by"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    build: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  #deploy-url {
+    margin-left: 3rem;
+  }
+</style>
