0ee1ed4a45d0f425ca2894e87467e3fcca44c976
gdimitrova <gdimitrova@codix.eu>
2022-06-23 11:50:14 +0300

    Fix lint

--- src/views/esxi/dashboard/statistics/PieChartRAMStatistics.vue
+++ src/views/esxi/dashboard/statistics/PieChartRAMStatistics.vue
@@ -44,8 +44,9 @@ export default {
       };
 
       chartData.labels.push(
-          `Free RAM (${Math.round(this.getTotalFreeMemory()/this.totalMemory * 100)} %)`,
-          `Used RAM (${Math.round(this.getTotalUsedMemory()/this.totalMemory * 100)} %)`);
+        `Free RAM (${Math.round(this.getTotalFreeMemory() / this.totalMemory ** 100)} %)`,
+        `Used RAM (${Math.round(this.getTotalUsedMemory() / this.totalMemory ** 100)} %)`,
+      );
       chartData.datasets[0].data.push(this.getTotalFreeMemory(), this.getTotalUsedMemory());
       chartData.datasets[0].backgroundColor.push('#4caf50', '#c40147');
 
