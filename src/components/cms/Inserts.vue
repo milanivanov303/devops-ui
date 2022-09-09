2877dfd366963f8cf6a743ba6fc6accd7249c24b
akyoseva <akyoseva@codix.eu>
2022-09-09 20:12:44 +0300

    create add param modal

--- src/components/cms/Inserts.vue
+++ src/components/cms/Inserts.vue
@@ -9,15 +9,17 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(command, key) in modifications"
-            v-bind:key="key">
-          <td class="left-align">{{command.name}}</td>
+        <tr v-for="(modification, index) in modifications"
+            v-bind:key="index">
+          <td class="left-align">{{modification.name}}</td>
           <td>
-            <button class="btn-floating waves-effect waves-light left btn-small red"
-                    type="button"
-                    @click="$emit('remove', key)"
-                    data-target="action"><i class="material-icons">remove</i>
+            <button
+                class="btn-floating waves-effect waves-light left btn-small red"
+                type="button"
+                @click="$emit('remove', index)"
+                data-target="action"><i class="material-icons">remove</i>
             </button>
+            <slot name="actions-before" :modification="modification"></slot>
           </td>
         </tr>
       </tbody>
