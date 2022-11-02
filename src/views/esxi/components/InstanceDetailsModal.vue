2c421b43999984481300eac95d999b816dc1f36a
gdimitrova <gdimitrova@codix.eu>
2022-11-02 17:57:25 +0200

    Show instances' ssl

--- src/views/esxi/components/InstanceDetailsModal.vue
+++ src/views/esxi/components/InstanceDetailsModal.vue
@@ -5,7 +5,22 @@
     </template>
     <template v-slot:content>
       <ul class="collapsible popout">
-        <li class="active">
+        <li class="active" >
+          <div class="collapsible-header"><i class="material-icons">laptop</i>Main Detail</div>
+          <div class="collapsible-body">
+            <p><b>Home path: </b>{{ instance.home_path }}</p>
+            <p><b>Patch Config Path: </b>{{ instance.patch_config_path }}</p>
+          </div>
+        </li>
+        <li v-if="instance.ssl">
+          <div class="collapsible-header"><i class="material-icons">laptop</i>SSL</div>
+          <div class="collapsible-body">
+            <p><b>BE: </b><a class="tbl-link" :href="instance.ssl.BE">{{ instance.ssl.BE }}</a></p>
+            <p><b>FE: </b><a class="tbl-link" :href="instance.ssl.FE">{{ instance.ssl.FE }}</a></p>
+            <p><b>Status: </b> {{ instance.ssl.status }}</p>
+          </div>
+        </li>
+        <li v-if="instance.filesystem">
           <div class="collapsible-header"><i class="material-icons">cast</i>Filesystem</div>
           <div class="collapsible-body">
             <p><b>Size: </b> {{ instance.filesystem.size }}</p>
